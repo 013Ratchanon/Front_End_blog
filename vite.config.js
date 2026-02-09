cec7098f1f254d0b6a3179bbd01ebd62138ddc9b
013Ratchanon <dewtete@gmail.com>
2026-02-09 14:16:02 +0700

    deploy github pages with docker

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: "/Front_End_blog/",
+  plugins: [react(), tailwindcss()],
 });
